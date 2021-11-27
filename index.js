dbeb82dd72f3d64a83c2670553f4c326561ff682
berkovich <pozitif.ivme9@gmail.com>
2021-11-27 15:22:11 +0300

    Simple logging with timestamp Fs is used for writing on log file date-fns module is used for timestamp

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 var http = require('http');
 var fs = require("fs");
+var format = require("date-fns/format");
+
+const logDir = __dirname+'/logs'
+var infoLog = fs.createWriteStream(logDir + '/traffic.log', {flags : 'a+'});
+
+/* With simple logs comes simple logging
+* Writes to file and stdout
+* */
+const logger = function(log)
+{
+    console.log(log);
+    infoLog.write(log+'\n');
+}
 
 const port = 3000;
 const hostname = '127.0.0.1';
 
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
     const routeMap = {
-        '/': "We are in index page",
-        '/about': "We are in about page",
-        '/contact': "We are in contact page",
+        'GET' : {
+            '/': "This is index page",
+            '/about': "This is in about page",
+            '/contact': "This is in contact page",
+        }
     }
 
-    console.log(`${req.method} request at ${req.url}`)
-    if (req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-
-        res.write(routeMap[req.url]);
+    // console.log(requestLog)
+    // infoLog.write(requestLog+'\n');
+    logger(`${ format(new Date(), 'MM/dd/yyyy HH:mm') } ${req.method} request at ${req.url}`)
+    if (!!routeMap[req.method][req.url])
+    {
+        res.writeHead(200, { 
+            "Content-Type": "text/plain; charset=utf-8",
+        });
+        res.write(routeMap[req.method][req.url]);
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404);
+        res.write('page not found');
         res.end();
     }
 }).listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}/`);
+    // console.log(serverLog);
+    logger(`${ format(new Date(), 'MM/dd/yyyy HH:mm') } server running at http://${hostname}:${port}/` );
 });
