f9432fed6b1adacf1c57b3000124eb205ab77dab
berkovich <pozitif.ivme9@gmail.com>
2021-11-27 20:18:15 +0300

    A little tweak to readme a little fmt

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# week-2-assignment
+# Description
+A small http server with very simple routing table. This is an assignment for Getir Bootcamp.
+# Usage
+- Firstly, `npm install` will install the dependencies, which are very few
+- `npm start` wil start the project
+## Current end points
+- /
+- /about
+- /contact
+## Defalults
+Main end point: [http://localhost:3000/](http://localhost:3000/)
 
- Bir sonraki derse hazırlıklı gelebilmeniz adına sizlere ARGE ödevi vermeye karar verdik. Bu repo üzerinde klasörlerinizi/dosyalarınızı oluşturup çalışabilirsiniz. 
-
- ## Yapmanız gereken maddeler;
-
-* Http modülü ile bir web server oluşturalım.
-* Client tarafından gönderilen request’leri; talep edilen url’lere yönlendirip (Ana Sayfa, Hakkımızda vs.) ekranda “… sayfasındasınız”  gibi bir mesaj verelim.
-
-* Gelen her request’i FS modülünü kullanarak bir dosyada loglayalım.
-
-* Tanımlı olmayan url’ler için gelen her request’i de 404 uyarısı ile yönlendirip mesaj gösterelim.
-
-
-NOTE: Node modules gibi büyük dosyaları github'a pushlamamanız için bir .gitignore dosyası eklemeyi de unutmayın.
-
-
-
+`./logs/traffic.log` stores the requests and server start status
--- index.js
+++ index.js
@@ -2,48 +2,43 @@ var http = require('http');
 var fs = require("fs");
 var format = require("date-fns/format");
 
-const logDir = __dirname+'/logs'
-var infoLog = fs.createWriteStream(logDir + '/traffic.log', {flags : 'a+'});
-
-/* With simple logs comes simple logging
-* Writes to file and stdout
-* */
-const logger = function(log)
-{
-    console.log(log);
-    infoLog.write(log+'\n');
-}
+const logDir = __dirname + '/logs'
+var infoLog = fs.createWriteStream(logDir + '/traffic.log', { flags: 'a+' }); // append at the end of the log file
 
 const port = 3000;
 const hostname = '127.0.0.1';
 
-http.createServer((req, res) => {
-    const routeMap = {
-        'GET' : {
+/* 
+* With simple logs comes simple logging
+* Writes to file and stdout
+* */
+const logger = function(log) {
+    console.log(log);
+    infoLog.write(log + '\n');
+}
+
+http.createServer((req, res) => { // here be a server
+    const routes = {
+        'GET': {
             '/': "This is index page",
             '/about': "This is in about page",
             '/contact': "This is in contact page",
         }
     }
 
-    // console.log(requestLog)
-    // infoLog.write(requestLog+'\n');
-    logger(`${ format(new Date(), 'MM/dd/yyyy HH:mm') } ${req.method} request at ${req.url}`)
-    if (!!routeMap[req.method][req.url])
-    {
-        res.writeHead(200, { 
+    logger(`${format(new Date(), 'MM/dd/yyyy HH:mm')} ${req.method} request at ${req.url}`)
+    if (!!routes[req.method][req.url]) { // if route exists bang bang
+        res.writeHead(200, {
             "Content-Type": "text/plain; charset=utf-8",
         });
-        res.write(routeMap[req.method][req.url]);
+        res.write(routes[req.method][req.url]);
         res.end();
     }
-    else
-    {
+    else {
         res.writeHead(404);
         res.write('page not found');
         res.end();
     }
 }).listen(port, hostname, () => {
-    // console.log(serverLog);
-    logger(`${ format(new Date(), 'MM/dd/yyyy HH:mm') } server running at http://${hostname}:${port}/` );
+    logger(`${format(new Date(), 'MM/dd/yyyy HH:mm')} server running at http://${hostname}:${port}/`);
 });
